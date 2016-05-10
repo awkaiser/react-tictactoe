6aae096dbc7416fb7f0c29f78ac9dd2ea5752d30
August “Kai” Kaiser <augustkaiser@gmail.com>
2016-05-10 11:04:31 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path')
+
+module.exports = {
+  entry: {
+    app: './src/app.jsx'
+  },
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  }
+}
