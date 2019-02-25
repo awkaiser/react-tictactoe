4c49b0d86a6e8d86be64ff5ceb3ac11248438ba8
August "Kai" Kaiser <awkaiser@users.noreply.github.com>
2019-02-25 15:40:47 -0800

    Updates

---
+++ precache-manifest.c67882b8da3ae52d16c97382ccba1202.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "baf581c95e77173fb80d",
+    "url": "/react-tictactoe/static/js/runtime~main.baf581c9.js"
+  },
+  {
+    "revision": "fd3f1da7de4eb4751173",
+    "url": "/react-tictactoe/static/js/main.fd3f1da7.chunk.js"
+  },
+  {
+    "revision": "f299abfb5f3b7e895a3b",
+    "url": "/react-tictactoe/static/js/2.f299abfb.chunk.js"
+  },
+  {
+    "revision": "16cd13424df58df845ac63e94d1daa1a",
+    "url": "/react-tictactoe/index.html"
+  }
+];
