515ecfbc47a11d75f33526831076ce99646f9245
August "Kai" Kaiser <awkaiser@users.noreply.github.com>
2019-11-16 18:07:27 -0800

    Updates

--- precache-manifest.3ebaaf7569aa3c7d8b577580e7fdd294.js
+++ precache-manifest.3ebaaf7569aa3c7d8b577580e7fdd294.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9d4a9d6e7ea2de494e43f719c7bd050",
+    "revision": "104f07469a61df2fce8d492b8f5aeaea",
     "url": "/react-tictactoe/index.html"
   },
   {
-    "revision": "3f8fe173046254b5f17d",
+    "revision": "ced9df07a844af711816",
     "url": "/react-tictactoe/static/css/main.aaa93f45.chunk.css"
   },
   {
-    "revision": "fc47ea50f7b4f555fa53",
-    "url": "/react-tictactoe/static/js/2.9f2d6b32.chunk.js"
+    "revision": "a99ce27bbdebf129c183",
+    "url": "/react-tictactoe/static/js/2.d93f4720.chunk.js"
   },
   {
-    "revision": "3f8fe173046254b5f17d",
-    "url": "/react-tictactoe/static/js/main.3f641e19.chunk.js"
+    "revision": "ced9df07a844af711816",
+    "url": "/react-tictactoe/static/js/main.ae635a7e.chunk.js"
   },
   {
     "revision": "59d2da7f2fe49f1e1404",
